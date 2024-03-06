a73b5edbb1bb125c8ac8a8358ba03c0ce2ac64b0
sokaraku <soner.karakus1999@gmail.com>
2024-03-06 20:46:54 +0100

    Not much, better understanding of the mlx library now

--- src/mlx_test.c
+++ src/mlx_test.c
@@ -6,26 +6,51 @@
 /*   By: sokaraku <sokaraku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 18:05:32 by sokaraku          #+#    #+#             */
-/*   Updated: 2024/03/05 19:18:24 by sokaraku         ###   ########.fr       */
+/*   Updated: 2024/03/06 20:42:54 by sokaraku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main(void)
+typedef struct s_win
 {
-	void	*mlx_s;
-	void	*mlx_win;
-	void	*img;
-	
+	void	*mlx_ptr;
+	void	*win_ptr;
+	char	*addr;
+	int		x;
+	int		y;
 
-	mlx_s = mlx_init();
-	mlx_win = mlx_new_window(mlx_s, 920, 1080, "ftg");
-	// mlx_destroy_display(mlx_win);
-	mlx_loop(mlx_s);
-	mlx_destroy_display(mlx_s);
-	free(mlx_win);
-	free(mlx_s);
+}			t_win;
+
+// void	ft_put_pixel(t_win *win, int x, int y, int color)
+// {
+// 	char	*dst;
+// 	int		offset_x;
+// 	int		offset_y;
+// 	int		offset;
+
+// 	offset_x = (win->bpp) / 8 * x;
+// 	offset_y = (win->line_length) * (win->bpp / 8);
+// 	offset = offset_x + offset_y;
+// 	dst = ;
+// 	mlx_new_image
+// }
+
+int	main(void)
+{
+	t_win	mlx;
+	void	*ptr;
+	
+	mlx.mlx_ptr = mlx_init();
+	if (!mlx.mlx_ptr)
+		exit(-1);
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 1920, 1080, "ftg");
+	if (!mlx.win_ptr)
+		return (mlx_destroy_display(mlx.mlx_ptr), free(mlx.mlx_ptr),
+			exit(EXIT_FAILURE), 1);
+	ptr = mlx_xpm_file_to_image(mlx.mlx_ptr, "gamepad.xpm", &mlx.x, &mlx.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, ptr, mlx.x, mlx.y);
+	mlx_loop(mlx.mlx_ptr);
 }
 
 // typedef struct	s_data {
@@ -51,11 +76,11 @@ int main(void)
 // 	t_data	img;
 
 // 	mlx = mlx_init();
-// 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-// 	img.img = mlx_new_image(mlx, 1920, 1080);
+// 	mlx_win = mlx_new_window(mlx, 190, 180, "Hello world!");
+// 	img.img = mlx_new_image(mlx, 540, 190);
 // 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 // 								&img.endian);
-// 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+// 	my_mlx_pixel_put(&img, , 5, 0x00FF0000);
 // 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 // 	mlx_loop(mlx);
 // }
